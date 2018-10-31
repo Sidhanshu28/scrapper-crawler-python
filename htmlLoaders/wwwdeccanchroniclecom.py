52931655a85d5d993415b25d952bda73dcf6922b
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-10-31 18:02:59 +0530

    extracting html selectors text code added

--- htmlLoaders/wwwdeccanchroniclecom.py
+++ htmlLoaders/wwwdeccanchroniclecom.py
@@ -1,7 +1,20 @@
 
+
+params = {
+    "title":"div > div > a > h3",
+    "link" : "div > div > a::attr(href)",
+}
+
 def parsedeccanchronicle(logger, response):
-    page = response.url.split("/")[-2]
-    filename = 'news-%s.html' %(page)
-    with open(filename, 'wb') as f:
-        f.write(response.body)
-    logger.log('Saved file %s' % filename)
+    # page = response.url.split("/")[-2]
+    # filename = 'news-%s.html' %(page)
+    # with open(filename, 'wb') as f:
+    #     f.write(response.body)
+    # logger.log('Saved file %s' % filename)
+
+    for news in response.css('div.opinionLanding'):
+        yield {
+            "title": news.css(params['title']+"::text").extract(),
+            "link": news.css(params['link']+"::text").extract(),
+            "source": response.url
+        }
