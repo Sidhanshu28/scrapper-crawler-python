913732660811ed90751ea706cd55f72199e9634e
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-10-31 17:37:36 +0530

    calling function from url folder seperately

--- crawlers/spiders/scrapit.py
+++ crawlers/spiders/scrapit.py
@@ -1,5 +1,6 @@
 import scrapy
-import errorHandling
+from errorHandling import errback_httpbin
+from filterFunction import filter_out
 
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError
@@ -12,19 +13,19 @@ class ScrapSpider(scrapy.Spider):
         urls = [
            'http://www.dailyo.in/politics',
            'http://www.deccanchronicle.com/opinion',
-           "http://www.httpbin.org/",
         ]
         i = 0
         for url in urls:
             i = i + 1   
-            request = scrapy.Request(url=url, callback=self.parse)
+            request = scrapy.Request(url=url, callback=self.parse, errback=errback_httpbin)
             request.meta['counter'] = i
             yield request
             
     def parse(self, response):
         i = response.meta['counter']
-        page = response.url.split("/")[-2]
-        filename = 'news-%s-%d.html' %(page,i)
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        filter_out(self, response)
+        # page.split('.').join('-')
+        # filename = 'news-%s-%d.html' %(page,i)
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s' % filename)
