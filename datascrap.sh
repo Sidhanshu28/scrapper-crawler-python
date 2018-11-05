86d3d05c4aca9555ccaaab47d780cacf7e467901
Sidhanshu Monga <sidhanshumonga28@gmail.com>
2018-11-05 20:26:18 +0530

    Adding datascrap.sh file for cronjob (need to alter for other user)

---
+++ datascrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin #making it bash command
+
+#command to make sh script for cronjob
+cd /Users/sidhanshu/Desktop/GitHub/scrapper-crawler-python
+scrapy crawl scrapit
